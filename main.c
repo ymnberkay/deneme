799ec3386e41c9f12d784983149d18a4278e329c
berkay <200201003@kocaeli.edu.tr>
2020-12-25 19:50:10 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+    float yaricap,pi;
+    pi=3.14;
+    printf("Bana bir yaricap soyle \n");
+    scanf("%f",&yaricap);
+    printf("Cemberin alani : %.2f",pi*yaricap*yaricap);
+    return 0;
+}
